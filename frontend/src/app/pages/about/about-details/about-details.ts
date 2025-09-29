e27b4592818800068ddd1b34458ad89929b469f2
sarweshwaranrs-dev <sarweshwaran.rs2021@vitstudent.ac.in>
2025-09-29 12:38:26 +0530

    Added the detailed-about section

---
+++ frontend/src/app/pages/about/about-details/about-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-details',
+  imports: [],
+  templateUrl: './about-details.html',
+  styleUrl: './about-details.css'
+})
+export class AboutDetails {
+
+}
