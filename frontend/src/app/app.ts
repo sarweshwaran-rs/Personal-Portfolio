e2305ce5d30dd43b5cbe6545bf912179efe8a997
sarweshwaranrs-dev <sarweshwaran.rs2021@vitstudent.ac.in>
2025-09-29 12:34:42 +0530

    Added the about section to the app.ts

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { ContactForm } from "./contact-form/contact-form";
 import { Education } from "./pages/education/education";
 import { Skills } from "./pages/skills/skills";
+import { About } from "./pages/about/about";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ContactForm, Education, Skills],
+  imports: [RouterOutlet, ContactForm, Education, Skills, About],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
