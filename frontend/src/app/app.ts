bfe77e073db87313e69e59b6cbedc4b78a21666d
sarweshwaranrs-dev <sarweshwaran.rs2021@vitstudent.ac.in>
2025-09-30 23:45:50 +0530

    Added the Publications component to the app.ts

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -4,10 +4,11 @@ import { ContactForm } from "./contact-form/contact-form";
 import { Education } from "./pages/education/education";
 import { Skills } from "./pages/skills/skills";
 import { About } from "./pages/about/about";
+import { Publications } from "./pages/publications/publications";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ContactForm, Education, Skills, About],
+  imports: [RouterOutlet, ContactForm, Education, Skills, About, Publications],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
