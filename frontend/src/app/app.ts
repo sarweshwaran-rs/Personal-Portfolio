d05aea21267d7e6c8a8d1d1681c4a5c345436333
= <sarweshwaran.rs2021@vitstudent.ac.in>
2025-10-01 15:13:34 +0530

    Refactored the Ordering of Imports

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,14 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ContactForm } from "./contact-form/contact-form";
-import { Education } from "./pages/education/education";
-import { Skills } from "./pages/skills/skills";
 import { About } from "./pages/about/about";
+import { Skills } from "./pages/skills/skills";
+import { Education } from "./pages/education/education";
 import { Publications } from "./pages/publications/publications";
+import { Projects } from "./pages/projects/projects";
+import { ContactForm } from "./contact-form/contact-form";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ContactForm, Education, Skills, About, Publications],
+  imports: [RouterOutlet, About, Education, Skills, Publications, Projects,ContactForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
