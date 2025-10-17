fef870779c07e3bb69a5af27e5e065edfdcb5602
sarweshwaranrs-dev <sarweshwaran.rs2021@vitstudent.ac.in>
2025-10-17 22:02:39 +0530

    Removed the unknown skills:

--- frontend/src/app/pages/skills/skills.ts
+++ frontend/src/app/pages/skills/skills.ts
@@ -13,14 +13,12 @@ export class Skills {
   skills = [
     { name: "Python", image: "/assets/images/python.svg", link:"https://www.python.org/" },
     { name: "Java", image: "/assets/images/java.svg", link:"https://www.oracle.com/in/java/technologies/" },
-    { name: "Spring", image: "/assets/images/spring.svg", link:"https://spring.io/" },
     { name: "Spring Boot", image: "/assets/images/spring-boot.svg", link:"https://spring.io/projects/spring-boot" },
     { name: "JavaScript", image: "/assets/images/javascript.svg", link:"https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
     { name: "Node.js", image: "/assets/images/node.svg", link:"https://nodejs.org/en" },
     { name: "Express", image: "/assets/images/express.svg", link:"https://expressjs.com/" },
     { name: "React", image: "/assets/images/react.svg", link:"https://react.dev/" },
     { name: "HTML/CSS", image: "/assets/images/html.svg", link:"https://developer.mozilla.org/en-US/docs/Web/HTML/" },
-    { name: "AWS", image: "/assets/images/aws.svg", link:"https://aws.amazon.com/" },
     { name: "MySQL", image: "/assets/images/mysql.svg", link:"https://www.mysql.com/" },
     { name: "PostgreSQL", image: "/assets/images/postgresql.svg", link:"https://www.postgresql.org/" },
     { name: "MongoDB", image: "/assets/images/mongodb.svg", link:"https://www.mongodb.com/" },
@@ -28,8 +26,6 @@ export class Skills {
     { name: "Git", image: "/assets/images/git.svg", link:"https://git-scm.com/" },
     { name: "GitHub", image: "/assets/images/github.svg", link:"https://github.com/" },
     { name: "Docker", image: "/assets/images/docker.svg", link:"https://www.docker.com/" },
-    { name: "C", image: "/assets/images/c.svg", link:"https://www.w3schools.com/c/c_intro.php/" },
     { name: "C++", image: "/assets/images/cpp.svg", link:"https://cplusplus.com/" },
-    { name: "C# (Basics)", image: "/assets/images/csharp.svg", link:"https://learn.microsoft.com/en-us/dotnet/csharp/" }
   ];
 }
