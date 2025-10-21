2529f6b7a73f5f88cdff8ac4a734c7cd8bbd3e77
sarweshwaranrs-dev <sarweshwaran.rs2021@vitstudent.ac.in>
2025-10-21 14:33:07 +0530

    Refactored the skills sections

--- frontend/src/app/pages/skills/skills.ts
+++ frontend/src/app/pages/skills/skills.ts
@@ -4,28 +4,37 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
-  standalone:true,
+  standalone: true,
   imports: [CommonModule, SkillCard],
   templateUrl: './skills.html',
   styleUrl: './skills.css'
 })
 export class Skills {
   skills = [
-    { name: "Python", image: "/assets/images/python.svg", link:"https://www.python.org/" },
-    { name: "Java", image: "/assets/images/java.svg", link:"https://www.oracle.com/in/java/technologies/" },
-    { name: "Spring Boot", image: "/assets/images/spring-boot.svg", link:"https://spring.io/projects/spring-boot" },
-    { name: "JavaScript", image: "/assets/images/javascript.svg", link:"https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
-    { name: "Node.js", image: "/assets/images/node.svg", link:"https://nodejs.org/en" },
-    { name: "Express", image: "/assets/images/express.svg", link:"https://expressjs.com/" },
-    { name: "React", image: "/assets/images/react.svg", link:"https://react.dev/" },
-    { name: "HTML/CSS", image: "/assets/images/html.svg", link:"https://developer.mozilla.org/en-US/docs/Web/HTML/" },
-    { name: "MySQL", image: "/assets/images/mysql.svg", link:"https://www.mysql.com/" },
-    { name: "PostgreSQL", image: "/assets/images/postgresql.svg", link:"https://www.postgresql.org/" },
-    { name: "MongoDB", image: "/assets/images/mongodb.svg", link:"https://www.mongodb.com/" },
-    { name: "Firebase", image: "/assets/images/firebase.svg", link:"https://firebase.google.com/" },
-    { name: "Git", image: "/assets/images/git.svg", link:"https://git-scm.com/" },
-    { name: "GitHub", image: "/assets/images/github.svg", link:"https://github.com/" },
-    { name: "Docker", image: "/assets/images/docker.svg", link:"https://www.docker.com/" },
-    { name: "C++", image: "/assets/images/cpp.svg", link:"https://cplusplus.com/" },
+    { name: "Python", image: "/assets/images/python.svg", link: "https://www.python.org/", skill_type: "lang" },
+    { name: "Java", image: "/assets/images/java.svg", link: "https://www.oracle.com/in/java/technologies/", skill_type: "lang" },
+    { name: "JavaScript", image: "/assets/images/javascript.svg", link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript", skill_type: "lang" },
+    { name: "HTML/CSS", image: "/assets/images/html.svg", link: "https://developer.mozilla.org/en-US/docs/Web/HTML/", skill_type: "lang" },
+    { name: "C++", image: "/assets/images/cpp.svg", link: "https://cplusplus.com/", skill_type: "lang" },
+    { name: "Spring Boot", image: "/assets/images/spring-boot.svg", link: "https://spring.io/projects/spring-boot", skill_type: "F&L" },
+    { name: "Node.js", image: "/assets/images/node.svg", link: "https://nodejs.org/en", skill_type: "F&L" },
+    { name: "Express", image: "/assets/images/express.svg", link: "https://expressjs.com/", skill_type: "F&L" },
+    { name: "React", image: "/assets/images/react.svg", link: "https://react.dev/", skill_type: "F&L" },
+    { name: "MySQL", image: "/assets/images/mysql.svg", link: "https://www.mysql.com/", skill_type: "C&D" },
+    { name: "PostgreSQL", image: "/assets/images/postgresql.svg", link: "https://www.postgresql.org/", skill_type: "C&D" },
+    { name: "MongoDB", image: "/assets/images/mongodb.svg", link: "https://www.mongodb.com/", skill_type: "C&D" },
+    { name: "Firebase", image: "/assets/images/firebase.svg", link: "https://firebase.google.com/", skill_type: "C&D" },
+    { name: "Git", image: "/assets/images/git.svg", link: "https://git-scm.com/", skill_type: "T&O" },
+    { name: "GitHub", image: "/assets/images/github.svg", link: "https://github.com/", skill_type: "T&O" },
+    { name: "Docker", image: "/assets/images/docker.svg", link: "https://www.docker.com/", skill_type: "T&O" },
   ];
+
+  get groupedSkills() {
+    return {
+      lang: this.skills.filter(s => s.skill_type === "lang"),
+      FandL: this.skills.filter(s => s.skill_type === "F&L"),
+      CandD: this.skills.filter(s => s.skill_type === "C&D"),
+      TandO: this.skills.filter(s => s.skill_type === "T&O"),
+    };
+  }
 }
