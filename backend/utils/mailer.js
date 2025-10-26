668266f7c1be0a8d54aec4eb1107bacbf58b7f02
sarweshwaranrs-dev <sarweshwaran.rs2021@vitstudent.ac.in>
2025-10-26 06:22:09 +0530

    Refactored the utils to support the gmailapi from its service

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,25 +1,41 @@
-const mailer = require('nodemailer');
-const dotenv = require('dotenv');
+const authorize = require("../services/googleApiAuthService.js");
+const sendEmail = require("../services/gmailApiService.js");
 
-dotenv.config();
+async function mailer(to, subject, html) {
+    try {
+        const auth = await authorize();
+        return await sendEmail(auth, to, subject, html);
+    } catch (error) {
+        console.error("Mailer Error: ", error);
+        throw error;
+        
+    }
+}
 
-const transporter = mailer.createTransport({
-    service:"gmail",
-    auth:{
-        user: process.env.GOOGLE_GMAIL_USER,
-        pass: process.env.GOOGLE_APP_PASSWORD,
-    },
-});
+module.exports = mailer;
 
-//console.log(`Transporter: ${transporter}`);
+// const mailer = require('nodemailer');
+// const dotenv = require('dotenv');
 
-async function sendMail(to, subject, html) {
-    await transporter.sendMail({
-        from: `"Portfolio Contact" <${process.env.GOOGLE_GMAIL_USER}>`,
-        to,
-        subject,
-        html
-    });
-};
+// dotenv.config();
 
-module.exports = sendMail;
+// const transporter = mailer.createTransport({
+//     service:"gmail",
+//     auth:{
+//         user: process.env.GOOGLE_GMAIL_USER,
+//         pass: process.env.GOOGLE_APP_PASSWORD,
+//     },
+// });
+
+// //console.log(`Transporter: ${transporter}`);
+
+// async function sendMail(to, subject, html) {
+//     await transporter.sendMail({
+//         from: `"Portfolio Contact" <${process.env.GOOGLE_GMAIL_USER}>`,
+//         to,
+//         subject,
+//         html
+//     });
+// };
+
+// module.exports = sendMail;
